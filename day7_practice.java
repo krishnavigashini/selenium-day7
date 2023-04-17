4b282a8541b68447228839ccbc0041847828971b
Krishna Vigashini <117428931+krishnavigashini@users.noreply.github.com>
2023-04-17 21:36:25 +0530

    Add files via upload

---
+++ day7_practice.java
@@ -0,0 +1,26 @@
+package day_7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class day7_practice {
+	@Test
+		public void practice()
+		{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver;
+		ChromeOptions co=new ChromeOptions();
+		co.addArguments("--remote-allow-origins=*");
+		driver=new ChromeDriver(co);
+		driver.get("https://demo.wpeverest.com/user-registration/guest-registration-form/");
+		String exp="Guest Registration Form-User Registration";
+		String actual=driver.getTitle();
+		System.out.println(actual);
+		Assert.assertEquals(exp, actual);
+		}
+}
